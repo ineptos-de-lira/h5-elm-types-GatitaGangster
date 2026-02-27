d52e152d7ff5df306cb92e4d980415ad53266943
GatitaGangster <up240219@alumnos.upa.edu.mx>
2026-02-27 14:41:50 -0600

    Tarea 5 UP240219

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,56 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+evaluarNota : Float -> GradeStatus
+evaluarNota nota =
+    if nota < 0 then
+        Pending
+
+    else if nota >= 7 then
+        Approved
+
+    else
+        Failed
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade listaDeNotas =
+    List.map evaluarNota listaDeNotas
+
+
+
+-->  E2
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction estado =
+    case estado of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction listaCadenas =
+    List.map airplaneScheduleAction listaCadenas
